a1018b8136ed7efcf297e778d92e94c5088f898d
ubkbek <ulugbekjovliyev01001@gmail.com>
2022-03-11 00:29:16 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,80 @@
+// object of users
+var users = {
+    "Akmal": {
+        name: "Akmal",
+        surname: "O'ktamov",
+        age: 27,
+        salary: 800
+    },
+
+    "Oybek": {
+        name: "Oybek",
+        surname: "sattorov",
+        age: 36,
+        salary: 1500
+    },
+
+    "Umid": {
+        name: "Umid",
+        surname: "Abdushukurov",
+        age: 21,
+        salary: 400
+    }
+}
+
+
+// function avarage age
+function avarageAge(object) {
+    var s = 0, k = 0;
+    for (var key in object) {
+        k++;
+        s += object[key].age;
+    }
+    return s / k;
+}
+
+
+// function sum salary
+function sumSalary(object) {
+    var s = 0;
+    for (var key in object) {
+        s += object[key].salary;
+    }
+    return s;
+}
+
+
+//take out to console
+console.log(sumSalary(users));
+console.log(avarageAge(users));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// function avarageAge(item2) {
+
+  	// Shu yerga kod yozasiz
+
+    // return //Bu yerga userlarni o'rtasha yoshini chiqarasiz
+// }
+
+
+// console'dagi natija
+// console.log(sumSalary(users));
+//--> 2700
+
+// console.log(avarageAge(users));
+//--> 28
